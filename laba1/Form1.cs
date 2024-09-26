de58b515c208407f655e9409536351f31a45887d
Refenger <121453687+Refenger@users.noreply.github.com>
2024-09-26 12:44:56 +0900

    Add files via upload

---
+++ laba1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba1
+{
+    public partial class Form1 : Form
+    {
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ClientSize = new Size(400, 400);
+            this.Text = "Оранжевый круг";
+            SetFormToCircle();
+            this.Paint += new PaintEventHandler(MainForm_Paint);
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+            this.MouseUp += new MouseEventHandler(Form1_MouseUp);
+            button.Location = new Point(200 - button.Width/2, 200 - button.Height/2);
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = true;
+                dragCursorPoint = Cursor.Position;
+                dragFormPoint = this.Location;
+            }
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = false;
+            }
+        }
+        private void SetFormToCircle()
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new Region(path);
+        }
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush brush = Brushes.Orange;
+            int diameter = Math.Max(ClientSize.Width, ClientSize.Height);
+            g.FillEllipse(brush, -1, -1, diameter + 2, diameter + 2);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            this.Hide();
+            form2.Show();
+        }
+    }
+    
+}
